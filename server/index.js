9f2012e02d7fb692956bbc7746d12ea13e327566
Anatskiy Aleksey <uyiiio@gmail.com>
2015-09-23 11:28:34 +0300

    add admin panel; fix styles

--- server/index.js
+++ server/index.js
@@ -3,25 +3,45 @@
 'use strict';
 
 var render = require('./lib/render');
-var route = require('koa-route');
+var Router = require('koa-router');
 var koaJsonLogger = require('koa-json-logger');
+var session = require('koa-session');
+var flash = require('koa-flash');
 
-// var parse = require('co-body');
+var parse = require('co-body');
 var koa = require('koa');
 var app = koa();
 
+var csrf = require('koa-csrf');
+
 const DEFAULT_PORT = 8767;
 var port = process.env.PORT || DEFAULT_PORT;
+
+var router = new Router();
+var routerAuth = new Router();
 
 // "database"
 
 // var posts = [];
 
 // middleware
+//
+app.keys = process.env.APP_KEYS || 'test,lol';
+app.keys = app.keys.split(',');
+app.use(session(app));
+csrf(app);
+// app.use(csrf.middleware);
+app.use(flash());
+
 app.use(koaJsonLogger({
     name: 'aaleks.ru',
     path: '/var/log/aaleks'
 }));
+
+if (process.env.NODE_ENV === 'development') {
+    var serve = require('koa-static');
+    app.use(serve(`${__dirname}/../static/public/`, {maxage: 65356}));
+}
 
 // errors middleware
 app.use(function*(next) {
@@ -36,27 +56,80 @@ app.use(function*(next) {
 
 app.use(function* (next) {
     yield next;
-    if (this.status === 404) {
-        this.body = yield render('error', {msg: 'Sorry, but nothin\' is here.', status: this.status});
+    if (this.status >= 400 && this.status < 500) {
+        this.body = yield render('error', {requestedPath: this.request.path, status: this.status});
     }
 });
 
-if (process.env.NODE_ENV === 'development') {
-    var serve = require('koa-static');
-    app.use(serve(`${__dirname}/../static/public`, {maxage: 65356}));
-}
-
 /**
- * Post listing.
+ * Auth middlevare
  */
 
-// route definitions
+function *checkAuth(next) {
+    /*jshint validthis:true */
+    if (this.session.user) {
+        yield next;
+    } else {
+        this.redirect(router.url('login'));
+    }
+}
+
+
+/**
+ * App routes
+ */
+routerAuth.use(checkAuth);
+
 function *index() {
+    /*jshint validthis:true */
     this.body = yield render('index');
 }
 
-// route middleware
-app.use(route.get('/', index));
+function *login() {
+    /*jshint validthis:true */
+    if (this.session.user) {
+        this.redirect(routerAuth.url('dashboard'));
+    }
+
+    this.body = yield render('admin/login', {csrf: this.csrf, flash: this.flash});
+}
+
+function *logoff(next) {
+    /*jshint validthis:true */
+    this.session = null;
+    this.redirect(router.url('index'));
+    yield next;
+}
+
+function *dashboard() {
+    /*jshint validthis:true */
+    this.body = yield render('admin/dashboard', {user: this.session.user});
+}
+
+function *authorize() {
+    /*jshint validthis:true */
+    var body = yield parse(this);
+
+    if (body.login === 'sintell' && body.password === 'password') {
+        this.session.user = {login: 'sintell'};
+        this.redirect(routerAuth.url('dashboard'));
+    } else {
+        delete body.password;
+        delete body._csrf;
+        this.flash = {error: 'Wrong username or password', oldForm: body};
+        this.redirect(router.url('login'));
+    }
+}
+
+router.get('index', '/', index);
+router.get('login', '/login', login);
+routerAuth.get('dashboard', '/admin/dashboard', dashboard);
+
+router.post('/login', authorize);
+routerAuth.post('logoff', '/logoff', logoff);
+
+app.use(router.routes());
+app.use(routerAuth.routes());
 
 app.listen(port);
 console.log(`listening on port ${port}`);
